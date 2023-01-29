107f62cd652495fff609e6b47b801739e024e367
felipemarinhodev <felipmarinho@gmail.com>
2023-01-28 22:23:28 -0800

    a3.7 - recovering local data

--- public/socket-front-document.js
+++ public/socket-front-document.js
@@ -3,7 +3,9 @@ import { atualizarTextoEditor } from "./documento.js";
 const socket = io();
 
 export function selectDocument(name) {
-    socket.emit("selecionar_documento", name)
+    socket.emit("selecionar_documento", name, (text) => {
+        atualizarTextoEditor(text)
+    })
 }
 
 export function emitirTextoEditor(text, documentName) {
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,15 +1,37 @@
 import io from "./server.js";
 
+const documents = [
+    {
+        name: "JavaScript",
+        text: "texto de javascript..."
+    },
+    {
+        name: "Node",
+        text: "texto de node..."
+    },
+    {
+        name: "Socket.io",
+        text: "texto de socket.io..."
+    }
+]
+
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou!", socket.id);
 
-    socket.on("selecionar_documento", (documentName) => {
+    socket.on("selecionar_documento", (documentName, returnText) => {
         socket.join(documentName);
+        const document = findDocument(documentName);
+        if (document) {
+            returnText(document.text)
+        }
     });
 
     socket.on("texto_editor", (text, documentName) => {
         // socket.broadcast.emit("texto_editor_clientes", text); // emite para todos exceto para quem foi o emissor.
-
         socket.to(documentName).emit("texto_editor_clientes", text);
     })
 })
+
+function findDocument(name) {
+    return documents.find(doc => doc.name === name)
+}
