8d1bd7e2265502be16a6cf46816c73db850b8d09
felipemarinhodev <felipmarinho@gmail.com>
2023-01-28 20:31:31 -0800

    a2.4 - issuing events

--- src/socket-back.js
+++ src/socket-back.js
@@ -2,4 +2,8 @@ import io from "./server.js";
 
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou!", socket.id);
+
+    socket.on("texto_editor", (text) => {
+        console.log(text);
+    })
 })
