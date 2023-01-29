7b61adb5bc2182b70abf051f74b3175487bc86d4
felipemarinhodev <felipmarinho@gmail.com>
2023-01-28 19:24:18 -0800

    a1.5 - config server socket.io

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import url from 'url';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const port = process.env.port || 8080;
@@ -8,6 +10,12 @@ const port = process.env.port || 8080;
 const currentPath = url.fileURLToPath(import.meta.url);
 const publicDirectory = path.join(currentPath, "../..", "public");
 
-app.use(express.static(publicDirectory))
+app.use(express.static(publicDirectory));
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(port, () => console.log(`Server listen on port: ${port}`));
+
+const io = new Server(httpServer);
 
 app.listen(port, () => console.log(`Server listen on port: ${port}`));
