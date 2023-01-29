2ce4a16840012237dc721cf7f52dae67aed8c512
felipemarinhodev <felipmarinho@gmail.com>
2023-01-28 20:49:10 -0800

    a2.6 - from server to the clients

--- src/socket-back.js
+++ src/socket-back.js
@@ -4,6 +4,6 @@ io.on("connection", (socket) => {
     console.log("Um cliente se conectou!", socket.id);
 
     socket.on("texto_editor", (text) => {
-        console.log(text);
+        socket.broadcast.emit("texto_editor_clientes", text); // emite para todos exceto para quem foi o emissor.
     })
 })
